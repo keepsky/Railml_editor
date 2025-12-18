9878aecc575b08063ff46637a79b797d9328e676
keepsky <keepsky@gmail.com>
2025-12-18 19:10:26 +0900

    Selection: 캔버스 위의 아이템을 클릭하면 선택되며, 속성 패널에 반영됩니다. Switch/Signal: Switch는 오렌지색 삼각형, Signal은 빨간색 원으로 표시되며 드래그 앤 드롭으로 추가 가능합니다.

--- RailmlEditor/MainWindow.xaml.cs
+++ RailmlEditor/MainWindow.xaml.cs
@@ -1,23 +1,76 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RailmlEditor.ViewModels;
 
-namespace RailmlEditor;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace RailmlEditor
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+            this.DataContext = _viewModel;
+        }
+
+        private void Toolbox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                // Drag Data: Simple String for now, could be an Enum or Complex Object
+                string type = button.Content.ToString();
+                DragDrop.DoDragDrop(button, type, DragDropEffects.Copy);
+            }
+        }
+
+        private void MainDesigner_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string type = (string)e.Data.GetData(DataFormats.StringFormat);
+                Point dropPosition = e.GetPosition(MainDesigner);
+
+                BaseElementViewModel newElement = null;
+
+                if (type == "Track")
+                {
+                    newElement = new TrackViewModel
+                    {
+                        Id = $"Tr{_viewModel.Elements.Count + 1}",
+                        X = dropPosition.X,
+                        Y = dropPosition.Y,
+                        Length = 100 // Default Length
+                    };
+                }
+                else if (type == "Switch")
+                {
+                    newElement = new SwitchViewModel
+                    {
+                        Id = $"Sw{_viewModel.Elements.Count + 1}",
+                        X = dropPosition.X,
+                        Y = dropPosition.Y
+                    };
+                }
+                else if (type == "Signal")
+                {
+                    newElement = new SignalViewModel
+                    {
+                        Id = $"Sig{_viewModel.Elements.Count + 1}",
+                        X = dropPosition.X,
+                        Y = dropPosition.Y
+                    };
+                }
+
+                if (newElement != null)
+                {
+                    _viewModel.Elements.Add(newElement);
+                    _viewModel.SelectedElement = newElement; // Auto Select
+                }
+            }
+        }
     }
 }
---
+++ RailmlEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using RailmlEditor.Models;
+
+namespace RailmlEditor.ViewModels
+{
+    public abstract class BaseElementViewModel : ObservableObject
+    {
+        private double _x;
+        private double _y;
+        private string _id;
+
+        public double X
+        {
+            get => _x;
+            set => SetProperty(ref _x, value);
+        }
+
+        public double Y
+        {
+            get => _y;
+            set => SetProperty(ref _y, value);
+        }
+
+        public string Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        public abstract string TypeName { get; }
+    }
+
+    public class TrackViewModel : BaseElementViewModel
+    {
+        public override string TypeName => "Track";
+        
+        private double _length;
+        public double Length
+        {
+            get => _length;
+            set => SetProperty(ref _length, value);
+        }
+    }
+
+    public class SwitchViewModel : BaseElementViewModel
+    {
+        public override string TypeName => "Switch";
+        // Orientation, BranchAngle etc.
+    }
+
+    public class SignalViewModel : BaseElementViewModel
+    {
+        public override string TypeName => "Signal";
+        // Type, Aspect etc.
+    }
+
+    public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<BaseElementViewModel> Elements { get; } = new ObservableCollection<BaseElementViewModel>();
+
+        private BaseElementViewModel _selectedElement;
+        public BaseElementViewModel SelectedElement
+        {
+            get => _selectedElement;
+            set => SetProperty(ref _selectedElement, value);
+        }
+
+        public ICommand SelectCommand { get; }
+
+        public MainViewModel()
+        {
+            SelectCommand = new RelayCommand(param => 
+            {
+                if (param is BaseElementViewModel element)
+                {
+                    SelectedElement = element;
+                }
+            });
+
+            // Test Data
+            Elements.Add(new TrackViewModel { Id = "Tr01", X = 100, Y = 100, Length = 200 });
+        }
+    }
+}
